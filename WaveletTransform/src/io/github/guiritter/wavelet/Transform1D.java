04a44f2cc66f355c76152593a50aa1900a25bd1e
GuiR <GuiR@amaterasu>
2017-09-26 09:31:47 -0300

    initial

---
+++ WaveletTransform/src/io/github/guiritter/wavelet/Transform1D.java
@@ -0,0 +1,56 @@
+package io.github.guiritter.wavelet;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Guilherme Alan Ritter
+ */
+public final class Transform1D {
+
+    private final List<Double[]> detailList = new LinkedList<>();
+
+    /**
+     * Low pass synthesis filter;
+     */
+    private final double filterC[];
+
+    /**
+     * High pass synthesis filter;
+     */
+    private final double filterD[];
+
+    /**
+     * Low pass analysis filter;
+     */
+    private final double filterF[];
+
+    /**
+     * High pass analysis filter;
+     */
+    private final double filterG[];
+
+    private final double original[];
+
+    private double smooth[];
+
+    public Transform1D(double[] original, double[] filterC, double[] filterD, double[] filterF, double[] filterG, int J) {
+        this.original = new double[original.length]; System.arraycopy(original, 0, this.original, 0, original.length);
+        this.filterC  = new double[filterC .length]; System.arraycopy(filterC , 0, this.filterC , 0, filterC .length);
+        this.filterD  = new double[filterD .length]; System.arraycopy(filterD , 0, this.filterD , 0, filterD .length);
+        this.filterF  = new double[filterF .length]; System.arraycopy(filterF , 0, this.filterF , 0, filterF .length);
+        this.filterG  = new double[filterG .length]; System.arraycopy(filterG , 0, this.filterG , 0, filterG .length);
+    }
+
+    public static void main(String args[]) {
+        double x = 1 / Math.sqrt(2);
+        Transform1D transform1D = new Transform1D(
+         new double[]{-2, 1, 3, 2, -3, 4},
+         new double[]{ x,  x},
+         new double[]{ x, -x},
+         new double[]{ x,  x},
+         new double[]{-x,  x},
+         1);
+    }
+}
